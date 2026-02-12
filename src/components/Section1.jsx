0e71101719c2246b9948d2078e6d1f1f373573bc
Shivam Bhardwaj <115487061+shivam-bhardwaj9@users.noreply.github.com>
2026-02-12 21:57:43 +0530

    Fix LeftContent import case and video path

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Leftcontent from './LeftContent'
+import LeftContent from './LeftContent'
+import video from '.src/assets/asset-ed557b88.mp4'
+
 
 const Section1 = () => {
     return (
         <div className='h-200 flex justify-between items-center pt-30 '>
-            <video src="src/assets/asset-ed557b88.mp4" autoPlay muted loop className='w-screen h-250 object-cover absolute z-[-1]'></video>
+            <video src={video} autoPlay muted loop className='w-screen h-250 object-cover absolute z-[-1]'></video>
             <LeftContent />
             
         </div>
