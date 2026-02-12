acb1d35b2bcfa0edb3cf576bc8ffbac18ed183ef
Shivam Bhardwaj <115487061+shivam-bhardwaj9@users.noreply.github.com>
2026-02-12 22:14:18 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import video from '../assets/video.mp4'
 
 const Footer = () => {
     return (
         <div className='h-50 text-black flex items-center justify-center overflow-hidden relative'>
-            <video src="src/assets/video.mp4" loop autoPlay muted className='rotate-90 h-300 relative'></video>
+            <video src={video} loop autoPlay muted className='rotate-90 h-300 relative'></video>
             <h1 className='absolute font-bold '>© 2026 Shivam Bhardwaj. All rights reserved. <br /><span className='ml-20 z-10 absolute'>Made With Love ❤</span></h1>
                 
         </div>
